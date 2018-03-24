a275601712593589ebe7d4924d6edf14c8020968
Mina Demian <mina@minademian.com>
2018-03-25 00:39:16 +0100

    chore: helper function for the debugz

--- includes/common/lib-functions.php
+++ includes/common/lib-functions.php
@@ -10,3 +10,15 @@ if (!function_exists('write_log')) {
         }
     }
 }
+
+if (!function_exists('debugvar')) {
+    function debugvar ( $log )  {
+        if ( true === WP_DEBUG ) {
+            if ( is_array( $log ) || is_object( $log ) ) {
+                echo '<pre>';print_r( $log );echo '</pre>';
+            } else {
+                error_log( $log );
+            }
+        }
+    }
+}
