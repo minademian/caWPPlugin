edb4a8eea6668ec3169759f8f5663b85484b41f3
Mina Demian <mina@minademian.com>
2018-03-25 18:42:54 +0200

    feat: styles and scripts for admin view

---
+++ admin/js/ca-worldapi-admin.js
@@ -0,0 +1,16 @@
+(function( $ ) {
+	'use strict';
+	$.initMap = function() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+  };
+
+	$.initMap();
+})( jQuery );
